f0e569c829eb50cba644698e4ec51be2eda0a874
KevinGage <dudeman02379@gmail.com>
2021-01-21 15:32:38 -0500

    added logic to play song

--- music-player/src/components/Player/Player.js
+++ music-player/src/components/Player/Player.js
@@ -1,3 +1,4 @@
+import { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
@@ -5,7 +6,13 @@ import {
   faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-function Player() {
+function Player({ currentSong }) {
+  const audioRef = useRef(null);
+
+  const playSongHandler = () => {
+    audioRef.current.play();
+  };
+
   return (
     <div className="player">
       <div className="time-control">
@@ -15,13 +22,19 @@ function Player() {
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
-        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          size="2x"
+          icon={faPlay}
+        />
         <FontAwesomeIcon
           className="skip-forward"
           size="2x"
           icon={faAngleRight}
         />
       </div>
+      <audio ref={audioRef} src={currentSong.audio} />
     </div>
   );
 }

d33e6cb1dd446fe8a3598fc8f09aff1a5d78c579
KevinGage <dudeman02379@gmail.com>
2021-01-21 21:45:46 -0500

    added auto play when switching songs

--- music-player/src/components/LibrarySong/LibrarySong.js
+++ music-player/src/components/LibrarySong/LibrarySong.js
@@ -1,6 +1,17 @@
-function LibrarySong({ song, setCurrentSong }) {
+function LibrarySong({ song, setCurrentSong, audioRef, isPlaying }) {
   const songSelectHandler = () => {
     setCurrentSong(song);
+
+    // starts audio when switching songs if player is playing
+    // has to use a promise to make sure song is loaded before playing
+    if (isPlaying) {
+      const playPromise = audioRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.then((audio) => {
+          audioRef.current.play();
+        });
+      }
+    }
   };
 
   return (

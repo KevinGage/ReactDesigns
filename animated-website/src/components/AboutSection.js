e6f78b5d22c833e60c1eae02c1973b3e092690af
KevinGage <dudeman02379@gmail.com>
2021-01-26 16:11:35 -0500

    moved styled components to seperate file and imported into about section

--- animated-website/src/components/AboutSection.js
+++ animated-website/src/components/AboutSection.js
@@ -1,5 +1,10 @@
 import home1 from "../img/home1.png";
-import styled from "styled-components";
+import {
+  StyledAbout,
+  StyledDescription,
+  StyledImage,
+  StyledHide,
+} from "../styles.js";
 
 const AboutSection = () => {
   return (
@@ -31,32 +36,4 @@ const AboutSection = () => {
   );
 };
 
-//Styled Components
-const StyledAbout = styled.div`
-  min-height: 90vh;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: 5rem 10rem;
-  color: white;
-`;
-const StyledDescription = styled.div`
-  flex: 1;
-  padding-right: 5rem;
-  h2 {
-    font-weight: lighter;
-  }
-`;
-const StyledImage = styled.div`
-  flex: 1;
-  overflow: hidden;
-  img {
-    width: 100%;
-    height: 80vh;
-    object-fit: cover;
-  }
-`;
-const StyledHide = styled.div`
-  overflow: hidden;
-`;
 export default AboutSection;

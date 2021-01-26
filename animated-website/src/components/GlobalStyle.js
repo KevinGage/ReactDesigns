742fe6bc2a500495329a21f6b55f84393b4b0ff2
KevinGage <dudeman02379@gmail.com>
2021-01-26 16:05:18 -0500

    added more global styles

--- animated-website/src/components/GlobalStyle.js
+++ animated-website/src/components/GlobalStyle.js
@@ -10,6 +10,7 @@ const GlobalStyle = createGlobalStyle`
 body{
   background: #1b1b1b;
 }
+
 button{
   font-weight: bold;
   font-size: 1.1rem;
@@ -23,6 +24,35 @@ button{
     background-color: #23d997;
     color: white;
   }
+}
+
+h2{
+  font-weight: lighter;
+  font-size: 4rem;
+}
+
+h3{
+  color: white;
+}
+
+h4 {
+  font-weight: bold;
+}
+
+span {
+  font-weight: bold;
+  color: #23d997;
+}
+
+a{
+  font-size: 1.1.rem;
+}
+
+p{
+  padding: 3rem 0rem;
+  color: #ccc;
+  font-size: 1.4rem;
+  line-height: 150%;
 }
 `;
 

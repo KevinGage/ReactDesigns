fc1ab72fc1cdcdf727e1b263d15c4612106cefa3
KevinGage <dudeman02379@gmail.com>
2021-01-28 14:01:26 -0500

    added framer animation wrapper to app

--- animated-website/src/App.js
+++ animated-website/src/App.js
@@ -3,30 +3,39 @@ import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
 import MovieDetail from "./pages/MovieDetail";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useLocation } from "react-router-dom";
 
 //Import global style
 import GlobalStyle from "./components/GlobalStyle";
 
+//Required for exit page animations
+import { AnimatePresence } from "framer-motion";
+
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Switch>
-        <Route path="/" exact>
-          <AboutUs />
-        </Route>
-        <Route path="/work" exact>
-          <OurWork />
-        </Route>
-        <Route path="/work/:id">
-          <MovieDetail />
-        </Route>
-        <Route path="/contact">
-          <ContactUs />
-        </Route>
-      </Switch>
+      {/* exitBeforeEnter tells framer motion to finish exit animation before starting enter animation */}
+      <AnimatePresence exitBeforeEnter>
+        {/* location and key required for framer motion exit animations to work */}
+        <Switch location={location} key={location.pathname}>
+          <Route path="/" exact>
+            <AboutUs />
+          </Route>
+          <Route path="/work" exact>
+            <OurWork />
+          </Route>
+          <Route path="/work/:id">
+            <MovieDetail />
+          </Route>
+          <Route path="/contact">
+            <ContactUs />
+          </Route>
+        </Switch>
+      </AnimatePresence>
     </div>
   );
 }

a650c083690af77a877d9fbf2e81dfb170803bc8
KevinGage <dudeman02379@gmail.com>
2021-01-29 12:48:35 -0500

    styled and animated contactus page

--- animated-website/src/pages/ContactUs.js
+++ animated-website/src/pages/ContactUs.js
@@ -1,17 +1,73 @@
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import { pageAnimation, titleAnim } from "../animation";
+import styled from "styled-components";
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <SyledContact
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
     >
-      <h1>contact us</h1>
-    </motion.div>
+      <StyledTitle>
+        <StyledHide>
+          <motion.h2 variants={titleAnim}>Get in touch.</motion.h2>
+        </StyledHide>
+      </StyledTitle>
+      <div>
+        <StyledHide>
+          <StlyedSocial variants={titleAnim}>
+            <StyledCircle />
+            <h2>Send Us A Message</h2>
+          </StlyedSocial>
+        </StyledHide>
+        <StyledHide>
+          <StlyedSocial variants={titleAnim}>
+            <StyledCircle />
+            <h2>Send An Email</h2>
+          </StlyedSocial>
+        </StyledHide>
+        <StyledHide>
+          <StlyedSocial variants={titleAnim}>
+            <StyledCircle />
+            <h2>Social Media</h2>
+          </StlyedSocial>
+        </StyledHide>
+      </div>
+    </SyledContact>
   );
 };
+
+const SyledContact = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  background: #fff;
+  min-height: 90vh;
+`;
+
+const StyledTitle = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+
+const StyledHide = styled.div`
+  overflow: hidden;
+`;
+
+const StyledCircle = styled.div`
+  border-radius: 50%;
+  width: 3rem;
+  height: 3rem;
+  background: #353535;
+`;
+
+const StlyedSocial = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2 {
+    margin: 2rem;
+  }
+`;
 
 export default ContactUs;

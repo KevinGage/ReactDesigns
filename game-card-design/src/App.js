bf82e3a724238d258604d2d29ef7c47136658b03
KevinGage <dudeman02379@gmail.com>
2021-02-10 11:00:03 -0500

    started simple navbar

--- game-card-design/src/App.js
+++ game-card-design/src/App.js
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
 import GlobalStyles from "./componenets/GlobalStyles";
 import { Route } from "react-router-dom";
+import Nav from "./componenets/Nav";
 
 function App() {
   return (
     <div>
       <GlobalStyles />
+      <Nav />
       <Route path={["/game/:id", "/"]}>
         <Home />
       </Route>
